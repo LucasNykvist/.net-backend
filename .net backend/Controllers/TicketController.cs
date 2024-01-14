2c4caf0d3bf6e01acb2356cf64e4f30ef907af4c
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-14 12:41:33 +0100

    Controller

---
+++ .net backend/Controllers/TicketController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+using Services;
+namespace _net_backend;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TicketController
+{
+    // Dependency Injection
+    private readonly ITicketService _ticketService;
+
+    public TicketController(ITicketService ticketService)
+    {
+        _ticketService = ticketService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTicketAsync([FromBody] Ticket ticket)
+    {
+        var createdTicket = await _ticketService.CreateTicketAsync(ticket);
+        return new CreatedAtRouteResult("GetTicketById", new { id = createdTicket.Id }, createdTicket);
+    }
+
+}
