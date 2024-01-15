28d568f1b6450ac3ee756c3d0a9da39d42e2d504
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-01-15 16:53:19 +0100

    Remove unused folder from DTOs.csproj

---
+++ DTOs/UpdateTicketDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace DTOs;
+public class UpdateTicketDTO
+{
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Status { get; set; }
+    public string? Type { get; set; }
+    public string? Priority { get; set; }
+    public string? Project { get; set; }
+    public string? Assignee { get; set; }
+    public string? Reporter { get; set; }   
+}
