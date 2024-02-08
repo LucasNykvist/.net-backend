89280bb4d9903258f61b95ba9ce87bdf953836fe
Lucas Nykvist <lucasnykvist01@gmail.com>
2024-02-08 20:46:57 +0100

    Book

---
+++ Models/DatabaseModels/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Models;
+public class Book
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public string? Author { get; set; }
+    public string? Genre { get; set; }
+    public string? Publisher { get; set; }
+    public DateTime? PublishDate { get; set; }
+    public string? Description { get; set; }
+    public string? ISBN { get; set; }
+    public string? Language { get; set; }
+    public int? Pages { get; set; }
+}
